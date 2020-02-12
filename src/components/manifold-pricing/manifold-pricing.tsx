7a9f3e0871b58bcbcbba6f23fdde9ced92812ddc
terrysmithdc <terry@designcollider.com>
2020-02-12 11:58:33 -0400

    Responsive table round 2

--- src/components/manifold-pricing/manifold-pricing.tsx
+++ src/components/manifold-pricing/manifold-pricing.tsx
@@ -6,31 +6,34 @@ import { Component, h } from '@stencil/core';
 })
 export class ManifoldPricing {
   render() {
+    const dummy = ['Pricing', 'Total', 'Banana', 'Orange', 'Weight', 'Count', 'Foo', 'Bar'];
     return (
       <div class="manifold-pricing">
-        <div class="manifold-pricing--table">
-          <div class="manifold-pricing--cell manifold-pricing--cell__thead manifold-pricing--mobile-hidden"></div>
-          <div class="manifold-pricing--cell manifold-pricing--cell__thead">Plan 1</div>
-          <div class="manifold-pricing--cell manifold-pricing--cell__thead">Plan 2</div>
-          <div class="manifold-pricing--cell manifold-pricing--cell__thead">Plan 3</div>
-
-          {Array(28)
+        <div class="mp--column mp--mobile-hidden">
+          <div class="mp--cell mp--cell__thead"></div>
+          {Array(5)
             .fill(0)
-            .map((_, i) => {
-              if (i % 4) {
-                return (
-                  <div class="manifold-pricing--cell manifold-pricing--cell__body">
-                    {i.toString()}
-                  </div>
-                );
-              }
-              return (
-                <div class="manifold-pricing--cell manifold-pricing--cell__thead manifold-pricing--mobile-hidden">
-                  {i.toString()}
-                </div>
-              );
-            })}
+            .map((_, i) => (
+              <div class="mp--cell mp--cell__thead">{dummy[i]}</div>
+            ))}
         </div>
+        {Array(3)
+          .fill(0)
+          .map((_, ii) => (
+            <div class="mp--column">
+              <div class="mp--cell mp--cell__thead mp--cell__bbs">Plan {ii + 1}</div>
+              {Array(5)
+                .fill(0)
+                .map((__, i) => (
+                  <div class="mp--cell mp--cell__body">
+                    {Math.random() > 0.5 && <div class="mp--mobile-only">{dummy[i]}</div>}
+                    <div>
+                      {ii.toString()} -- {i.toString()}
+                    </div>
+                  </div>
+                ))}
+            </div>
+          ))}
       </div>
     );
   }
