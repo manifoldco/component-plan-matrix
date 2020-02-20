aa3e8e2c2a3a5b9d0616551538effe6c8b1bb890
Drew Powers <dangodev@users.noreply.github.com>
2020-02-20 14:09:19 -0700

    Add cta-text & base-url props (#14)

--- stories/pricing.stories.js
+++ stories/pricing.stories.js
@@ -1,10 +1,13 @@
 import { storiesOf } from '@storybook/html';
-import { withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, text } from '@storybook/addon-knobs';
 import { withA11y } from '@storybook/addon-a11y';
 
 storiesOf('Manifold Pricing', module)
   .addDecorator(withKnobs)
   .addDecorator(withA11y)
   .add('manifold-plan-matrix', () => {
-    return `<manifold-plan-matrix></manifold-plan-matrix>`;
+    const cta = text('cta-text', 'Get Started');
+    const baseUrl = text('base-url', '/signup');
+
+    return `<manifold-plan-matrix base-url="${baseUrl}" cta-text="${cta}"></manifold-plan-matrix>`;
   });
