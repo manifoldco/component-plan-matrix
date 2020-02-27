102e202f8086ac5d32618a00c0c0e4d86010e294
terrysmithdc <terry@designcollider.com>
2020-02-27 11:33:55 -0400

    Adds graphql-url atrtibute to manifold-plan-matrix

--- src/components.d.ts
+++ src/components.d.ts
@@ -23,6 +23,7 @@ export namespace Components {
   interface ManifoldPlanMatrix {
     'baseUrl'?: string;
     'ctaText'?: string;
+    'graphqlUrl'?: string;
     'productLabel'?: string;
   }
   interface ManifoldThead {
@@ -77,6 +78,7 @@ declare namespace LocalJSX {
   interface ManifoldPlanMatrix {
     'baseUrl'?: string;
     'ctaText'?: string;
+    'graphqlUrl'?: string;
     'productLabel'?: string;
   }
   interface ManifoldThead {
