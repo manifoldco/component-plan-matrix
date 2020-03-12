12e12bb4bda9ba67de7e89339471653077aa601c
Sam Slotsky <sam@manifold.co>
2020-03-12 15:12:40 -0500

    Fix Happo, maybe

--- src/types/graphql.ts
+++ src/types/graphql.ts
@@ -1229,6 +1229,13 @@ export type ProductQuery = (
       & { edges: Array<(
         { __typename?: 'ProductConfigurableFeatureEdge' }
         & { node: (
+          { __typename?: 'ProductStringConfigurableFeature' }
+          & Pick<ProductStringConfigurableFeature, 'label' | 'displayName'>
+          & { featureOptions: Maybe<Array<(
+            { __typename?: 'ProductConfigurableFeatureOption' }
+            & Pick<ProductConfigurableFeatureOption, 'cost' | 'displayName' | 'value'>
+          )>> }
+        ) | (
           { __typename?: 'ProductBooleanConfigurableFeature' }
           & Pick<ProductBooleanConfigurableFeature, 'label' | 'displayName'>
           & { featureOptions: Maybe<Array<(
@@ -1249,13 +1256,6 @@ export type ProductQuery = (
                 & Pick<ProductFeatureCostTier, 'cost' | 'limit'>
               )> }
             ) }
-          )>> }
-        ) | (
-          { __typename?: 'ProductStringConfigurableFeature' }
-          & Pick<ProductStringConfigurableFeature, 'label' | 'displayName'>
-          & { featureOptions: Maybe<Array<(
-            { __typename?: 'ProductConfigurableFeatureOption' }
-            & Pick<ProductConfigurableFeatureOption, 'cost' | 'displayName' | 'value'>
           )>> }
         ) }
       )> }
