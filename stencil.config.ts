654369cd205d87186bac0d7031666cff4048d13e
Drew Powers <drew@pow.rs>
2020-03-31 17:54:20 -0600

    Fix Jest for SVGs

--- stencil.config.ts
+++ stencil.config.ts
@@ -65,6 +65,10 @@ export const config: Config = {
     },
   ],
   testing: {
+    moduleNameMapper: {
+      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+        '<rootDir>/__mocks__/fileMock.js',
+    },
     testPathIgnorePatterns: ['/node_modules/'],
     transformIgnorePatterns: ['node_modules/(?!@manifoldco/manifold-init)'],
     transform: {
