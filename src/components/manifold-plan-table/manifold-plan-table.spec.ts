68f98b88b1aa891fdc2590375fa9aa7d0cb496d7
Drew Powers <drew@pow.rs>
2020-05-15 08:51:08 -0600

    Fix test

--- src/components/manifold-plan-table/manifold-plan-table.spec.ts
+++ src/components/manifold-plan-table/manifold-plan-table.spec.ts
@@ -6,8 +6,14 @@ import { ManifoldPlanTable } from './manifold-plan-table';
 import mockLogDna from '../../mocks/graphql/product-logDna.json';
 import mockJawsDB from '../../mocks/graphql/product-jawsdb-mysql.json';
 
+// mock AbortController
+const globalAny = global as any;
+globalAny.AbortController = class AbortController {
+  abort() {}
+};
+
 const GRAPHQL_ENDPOINT = 'https://api.manifold.co/graphql';
-const REST_ENDPOINT = 'http://test.com/v1';
+const REST_ENDPOINT = 'begin:https://api.manifold.co/v1';
 const ANALYTICS_ENDPOINT = 'https://analytics.manifold.co/v1/events';
 
 interface Props {
@@ -75,45 +81,46 @@ describe(ManifoldPlanTable.name, () => {
     it('[version]: when latest is provided it is a latest query', async () => {
       fetchMock.mock(GRAPHQL_ENDPOINT, mockJawsDB);
       await setup({ productId: 'product-id', clientId: 'client-id', version: 'latest' });
-      const options = fetchMock.lastOptions();
-      expect(options.body).toContain('"latest":true');
+      const [[, query]] = fetchMock.calls().filter((call) => call[0] === GRAPHQL_ENDPOINT);
+      expect(query.body).toContain('"latest":true');
     });
 
     it('[version]: when version is provided it is not a latest query', async () => {
       fetchMock.mock(GRAPHQL_ENDPOINT, mockJawsDB);
       await setup({ productId: 'product-id', clientId: 'client-id', version: '1' });
-      const options = fetchMock.lastOptions();
-      expect(options.body).toContain('"latest":false');
+      const [[, query]] = fetchMock.calls().filter((call) => call[0] === GRAPHQL_ENDPOINT);
+      expect(query.body).toContain('"latest":false');
     });
+  });
 
-    describe('analytics', () => {
-      beforeEach(() => {
-        fetchMock.mock(GRAPHQL_ENDPOINT, mockLogDna);
+  describe('analytics', () => {
+    beforeEach(() => {
+      fetchMock.mock(REST_ENDPOINT, { cost: 3500, currency: 'USD' });
+      fetchMock.mock(GRAPHQL_ENDPOINT, mockLogDna);
+      fetchMock.mock(ANALYTICS_ENDPOINT, 200);
+    });
+    afterEach(fetchMock.restore);
+
+    it('click', async () => {
+      const productId = '789456123';
+      const clientId = '369258147';
+
+      const { page } = await setup({ productId, clientId });
+
+      const forms = page.root.querySelector<HTMLFormElement>('form');
+
+      forms.dispatchEvent(new Event('submit'));
+
+      const [[, analyticsRes]] = fetchMock.calls().filter(([url, body]) => {
+        return url === ANALYTICS_ENDPOINT && (body.body as string).includes('"name":"click"');
       });
+      const body = typeof analyticsRes?.body === 'string' && JSON.parse(analyticsRes.body);
+      const plan = mockLogDna.data.product.plans.edges[0].node;
 
-      it('click', async () => {
-        const productId = '789456123';
-        const clientId = '369258147';
-
-        const { page } = await setup({
-          productId,
-          clientId,
-        });
-
-        const forms = page.root.querySelector<HTMLFormElement>('form');
-
-        forms.dispatchEvent(new Event('submit'));
-
-        const [[, analyticsRes]] = fetchMock
-          .calls()
-          .filter((call) => call[0] === ANALYTICS_ENDPOINT);
-        const body = typeof analyticsRes?.body === 'string' && JSON.parse(analyticsRes.body);
-
-        expect(body.type).toContain('component-analytics');
-        expect(body.properties.version).toContain('<@NPM_PACKAGE_VERSION@>');
-        expect(body.properties.planId.length).toBeGreaterThan(0);
-        expect(body.properties.clientId.length).toBeGreaterThan(0);
-      });
+      expect(body.type).toContain('component-analytics');
+      expect(body.properties.version).toContain('<@NPM_PACKAGE_VERSION@>');
+      expect(body.properties.planId).toBe(plan.id);
+      expect(body.properties.clientId).toBe(clientId);
     });
   });
 });
